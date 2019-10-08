b8e407318a84a4d119737cfbba5269b92444a601
sdoublet <doubletsebastien@sfr.fr>
2019-10-08 12:12:03 +0200

    add first layout and depedencies

--- app/src/main/java/com/example/myrealestatemanager/MainActivity.java
+++ app/src/main/java/com/example/myrealestatemanager/MainActivity.java
@@ -1,14 +1,37 @@
 package com.example.myrealestatemanager;
 
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        configureStatusBar();
+    }
+
+    //Set color for status bar
+    private void configureStatusBar(){
+        if (Build.VERSION.SDK_INT>= Build.VERSION_CODES.LOLLIPOP){
+            Window window = getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(ContextCompat.getColor(this, R.color.colorPrimaryDark));
+        }
+    }
+
+    //configure drawer layout
+    private void configureDrawerLayout(){
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, )
     }
 }
---
+++ app/src/main/java/com/example/myrealestatemanager/SplashScreenActivity.java
@@ -0,0 +1,41 @@
+package com.example.myrealestatemanager;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    public static final int SLPASH_SCREEN_TIMEOUT = 5000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        configureStatusBar();
+        //Redirect to mainActivity after 3s
+        //Handler Post Delayed
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
+        }, SLPASH_SCREEN_TIMEOUT);
+    }
+
+    // TODO: 07/10/2019 mutualiser le configureStatusBar
+    //Set color for status bar
+    private void configureStatusBar() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(ContextCompat.getColor(this, R.color.colorPrimaryDark));
+        }
+    }
+}
