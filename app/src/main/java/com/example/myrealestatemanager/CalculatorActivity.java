940ec2804ae3bd56ca187e48234ab7d7d0bd9454
sdoublet <doubletsebastien@sfr.fr>
2019-10-29 15:03:24 +0100

    add credit simulator method

--- app/src/main/java/com/example/myrealestatemanager/CalculatorActivity.java
+++ app/src/main/java/com/example/myrealestatemanager/CalculatorActivity.java
@@ -3,12 +3,60 @@ package com.example.myrealestatemanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Locale;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class CalculatorActivity extends AppCompatActivity {
 
+    @BindView(R.id.price_edit)
+    EditText priceEdit;
+    @BindView(R.id.duration_edit)
+    EditText duration;
+    @BindView(R.id.taux_edit)
+    EditText taux;
+    @BindView(R.id.result_edit)
+    TextView result;
+    @BindView(R.id.result_credit)
+    Button resultCredit;
+    double p;
+    float t;
+    int d;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
+        ButterKnife.bind(this);
+        resultCredit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                p = Double.parseDouble(priceEdit.getText().toString());
+                t = Float.parseFloat(taux.getText().toString());
+                d = Integer.parseInt(duration.getText().toString());
+                double calcul =(calculResult(p, d, t));
+               // result.setText(Double.toString(calcul));
+                result.setText(String.format(Locale.getDefault(), "%.2f $/month", calcul));
+            }
+        });
+
+
+
+
+    }
+
+    private double calculResult(double price, int duration, float taux){
+        double ratePerMonth = (taux / 100.0) / 12.0;
+        int totalMonth = duration * 12;
+
+        double result =  (price * ratePerMonth) / (1 - Math.pow(1 + ratePerMonth, -totalMonth));
+
+        return  ((long)(result * 100) / 100.0);
     }
 }
--- app/src/main/java/com/example/myrealestatemanager/fragment_main.java
+++ app/src/main/java/com/example/myrealestatemanager/fragment_main.java
@@ -1,15 +1,16 @@
 package com.example.myrealestatemanager;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 
 /**
@@ -17,6 +18,7 @@ import android.view.ViewGroup;
  */
 public class fragment_main extends Fragment {
 
+    private Button buttonSimulator;
 
     public fragment_main() {
         // Required empty public constructor
@@ -26,12 +28,25 @@ public class fragment_main extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         View v = inflater.inflate(R.layout.fragment_main, container, false);
         NavHostFragment hostFragment = (NavHostFragment) getActivity().getSupportFragmentManager().findFragmentById(R.id.fragment);
         NavController navController = hostFragment.getNavController();
 
+        buttonSimulator = v.findViewById(R.id.button_simulator);
+        launchSimulatorActivity();
         // Inflate the layout for this fragment
         return v;
     }
 
+    private void launchSimulatorActivity(){
+        buttonSimulator.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), CalculatorActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
 }
